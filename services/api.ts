fac6f9e7f4d75bcd95b641070c2490b7f2fce8cb
Mounkam Charly Pierre <72751494+1998-el@users.noreply.github.com>
2026-07-15 10:38:30 +0100

    Update api.ts

--- services/api.ts
+++ services/api.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://heyama-h7gwjo6l6-charly-pierre-mounkams-projects.vercel.app";
 
 export interface ObjectItem {
   id: string;
